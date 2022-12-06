1e8d23115b71c988bdb2886894166eb4427561dd
Chris Klimas <christopherk@spotify.com>
2022-12-06 22:40:45 +0100

    Implemented day 4

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-use day3::sum_priorities;
-use day3::three_rucksack_priority;
-
 pub mod day1;
 pub mod day2;
 pub mod day3;
+pub mod day4;
 
 fn main() {
-    println!("Day3: {}", sum_priorities());
-    println!("Day3-2: {}", three_rucksack_priority());
+    println!("Day4: {}", day4::find_partial_overlapping());
 }
