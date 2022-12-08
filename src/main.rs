0133abc0659d7b1a429cb6bf370acc802a44395d
Chris Klimas <christopherk@spotify.com>
2022-12-08 22:29:39 +0100

    WIP day5 advent of code

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ pub mod day1;
 pub mod day2;
 pub mod day3;
 pub mod day4;
+pub mod day5;
 
 fn main() {
-    println!("Day4: {}", day4::find_partial_overlapping());
+    println!("Day5: {}", day5::get_top_stacks());
 }
